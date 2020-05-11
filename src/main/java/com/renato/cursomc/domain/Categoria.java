f6b62fedfe9e346368e8c8c7b9ce053ad5fb1d75
Renato Gondin <renatogondin1902@gmail.com>
2020-05-11 10:12:09 -0300

    adding exceptions for Categoria

--- src/main/java/com/renato/cursomc/domain/Categoria.java
+++ src/main/java/com/renato/cursomc/domain/Categoria.java
@@ -10,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Categoria implements Serializable {
 
@@ -25,8 +27,9 @@ public class Categoria implements Serializable {
 	
 	
 	//atributo referente aos produtos de uma categoria
+	@JsonManagedReference
 	@ManyToMany(mappedBy ="categorias")
-	List<Produto> produtos = new ArrayList<>();
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {
 		
--- src/main/java/com/renato/cursomc/domain/Produto.java
+++ src/main/java/com/renato/cursomc/domain/Produto.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 
 @Entity
 public class Produto implements Serializable {
@@ -28,12 +30,13 @@ public class Produto implements Serializable {
 	private Double preco;
 	
 	//produto tem várias categorias
+	@JsonBackReference
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA",
 	joinColumns = @JoinColumn(name = "produto_id"),
 	inverseJoinColumns = @JoinColumn(name = "categoria_id")
 	)
-	List<Categoria> categorias = new ArrayList<>();
+	private List<Categoria> categorias = new ArrayList<>();
 	
 	public Produto() {
 		
