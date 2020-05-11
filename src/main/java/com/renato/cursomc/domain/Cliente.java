e0fcd1432717ea3b8f65f6d063dfa72f0164ee5a
Renato Gondin <renatogondin1902@gmail.com>
2020-05-11 16:11:14 -0300

    endpoint clientes/{id}

--- src/main/java/com/renato/cursomc/domain/Cliente.java
+++ src/main/java/com/renato/cursomc/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.renato.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -32,6 +33,8 @@ public class Cliente implements Serializable{
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	
+	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
